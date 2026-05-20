1197420b929cfe882354d28908c51ef973e5e6c2
GIOVANNA SILVA <giovanna_silva151@estudante.sesisenai.org.br>
2026-05-20 20:47:43 -0300

    atv

---
+++ Atividades-avaliativas.js/arq.js
@@ -0,0 +1,12 @@
+const num1 = 121;
+const num2 = 123;
+const num3 = 11211;
+
+let xNum1 = null
+let xNum2 = null
+let xNum3 = null
+
+// Se o numero for IGUAL de tras pra frente -> log.true
+
+
+
--- Atividades-avaliativas.js/atv-avaliativa-2.5.js
+++ Atividades-avaliativas.js/atv-avaliativa-2.5.js
@@ -1,5 +1,16 @@
+//                              JOGO DA VELHA X/O
+
+//  1 - Como funciona o jogo?
+
+    //  ° O jogo tem 9 posições a serem ocupadas.
+    //  ° Se todas essas posições forem ocupadas, dara velha = sem vencedor ou empate.
+    //  ° Para ganhar a rodada é preciso ocupar uma das posições: diagonal, vertival ou horizontal.
+    //  ° Ao final da rodada vencedora o jogo será encerrado.
+
+
 const ler = require("readline-sync");
 
+// Criado variaveis para guardar os valores e criar o jogo da velha
 let tabuleiro = [
     ["-", "-", "-"],
     ["-", "-", "-"],
@@ -9,12 +20,14 @@ let tabuleiro = [
 let jogador = "X";
 let jogadas = 0;
 
+// Iniciando a estrutura com while
 while (true) {
 
     console.log("\n===== TABULEIRO =====");
 
     for (let i = 0; i < 3; i++) {
 
+// exibindo o tabuleiro
         console.log(
             tabuleiro[i][0] + " | " +
             tabuleiro[i][1] + " | " +
@@ -25,9 +38,11 @@ while (true) {
 
     console.log("\nJogador:", jogador);
 
+// variaveis criada para guardar o X e o O nas posições do tabuleiro
     let linha = ler.questionInt("Digite a linha (0 a 2): ");
     let coluna = ler.questionInt("Digite a coluna (0 a 2): ");
 
+// Em caso de posição ja ocupada ou invalida, usei if aninhado para verificar o mesmo e  assim retornar como "Posicao invalida!"
     if (
         linha < 0 || linha > 2 ||
         coluna < 0 || coluna > 2
@@ -45,9 +60,12 @@ while (true) {
 
     }
 
+// guardando o valor na linha e coluna da matriz e contando as rodadas
     tabuleiro[linha][coluna] = jogador;
 
     jogadas++;
+
+// IF abaixo: usado para acessar as posições de cada jogador para verificar um vencedor
 
     if (
         (
@@ -99,6 +117,7 @@ while (true) {
         )
     ) {
 
+// FOr para verificação do vencedor e exibir 
         console.log("\n===== TABULEIRO FINAL =====");
 
         for (let i = 0; i < 3; i++) {
@@ -117,6 +136,7 @@ while (true) {
 
     }
 
+// IF usado para verificar o empate e exibir 
     if (jogadas === 9) {
 
         console.log("\n===== TABULEIRO FINAL =====");
@@ -137,6 +157,7 @@ while (true) {
 
     }
 
+// abaixo codigo para alternar os jogadores (X / O)
     if (jogador === "X") {
 
         jogador = "O";
@@ -145,6 +166,6 @@ while (true) {
 
         jogador = "X";
 
-    }
+    };
 
-}
+};
